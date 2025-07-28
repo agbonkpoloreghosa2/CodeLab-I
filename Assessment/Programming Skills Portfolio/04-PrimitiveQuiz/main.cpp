5d64d829faf2157ae2aded890e0f9b9f5a73af83
DELL <agbonkpoloreghosa2@gmail.com>
2025-07-28 23:37:49 +0600

    "Primitive quiz to find correct and wrong answer of capitals"

---
+++ Assessment/Programming Skills Portfolio/04-PrimitiveQuiz/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+
+string toLower(string str) {
+    transform(str.begin(), str.end(), str.begin(), ::tolower);
+    return str;
+}
+
+int main() {
+
+    string questions[] = {
+        "What is the capital of France? ",
+        "What is the capital of the UK? ",
+        "What is the capital of Germany? ",
+        "What is the capital of Spain? ",
+        "What is the capital of Italy? "
+    };
+
+    string answers[] = {"paris", "london", "berlin", "madrid", "rome"};
+    int score = 0;
+
+    cout << "---- Primitive Quiz ----\n";
+
+// W3Schools https://www.w3schools.com/cpp/cpp_for_loop.asp
+
+    for (int i = 0; i < 5; i++) {
+        string userAnswer;
+
+        cout << "\nQuestion " << (i + 1) << ": " << questions[i];
+        getline(cin, userAnswer);
+
+        if (toLower(userAnswer) == answers[i]) {
+            cout << " Correct!\n";
+            score++;
+        } else {
+            cout << " Incorrect. The correct answer is " << answers[i] << ".\n";
+        }
+    }
+
+
+    cout << "\nQuiz Complete! Your score: " << score << "/5\n";
+
+    return 0;
+}
