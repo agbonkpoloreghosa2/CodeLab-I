ca99b8c567e85a71263b2c993c29345ed8c16383
DELL <agbonkpoloreghosa2@gmail.com>
2025-07-28 23:32:53 +0600

    "Print name, hometown and age to the console"

---
+++ Assessment/Programming Skills Portfolio/03-Biography/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string name;
+    string hometown;
+    int age;
+
+    cout << "Enter your full name: ";
+    cin >> name;
+
+    cout << "Enter your hometown: ";
+    cin >> hometown;
+
+    cout << "Enter your age: ";
+
+    while (!(cin >> age)) {
+        cout << "Invalid! Please enter a number for age: ";
+        cin.clear();
+        cin.ignore(1000, '\n');
+    }
+    cin.ignore();
+
+
+    cout << "\n---- Biography ----\n"
+        << "Name: " << name << '\n'
+        << "Hometown: " << hometown << '\n'
+        << "Age: " << age << '\n';
+
+    return 0;
+
+}
