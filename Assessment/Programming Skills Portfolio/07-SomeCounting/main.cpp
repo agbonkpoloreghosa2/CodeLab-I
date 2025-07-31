d0f045be1b8cc06d700194cb4bce05da3e2f747e
Eghosa Agbonkpolor <agbonkpoloreghosa2@gmail.com>
2025-07-31 18:07:32 +0100

    Update main.cpp

--- Assessment/Programming Skills Portfolio/07-SomeCounting/main.cpp
+++ Assessment/Programming Skills Portfolio/07-SomeCounting/main.cpp
@@ -3,39 +3,39 @@ using namespace std;
 
 int main() {
 
-    cout << "Counting 0 to 50:\n";
+    cout << "Counting 0 to 50:" << endl;
     for (int i = 0; i <= 50; i++) {
         cout << i << " ";
     }
-    cout << "\n\n";
+    cout << endl << endl;
 
 
-    cout << "Counting 50 to 0:\n";
+    cout << "Counting 50 to 0:" << endl;
     for (int i = 50; i >= 0; i--) {
         cout << i << " ";
     }
-    cout << "\n\n";
+    cout << endl << endl;
 
 
-    cout << "Counting 30 to 50:\n";
+    cout << "Counting 30 to 50:" << endl;
     for (int i = 30; i <= 50; i++) {
         cout << i << " ";
     }
-    cout << "\n\n";
+    cout << endl << endl;
 
 
-    cout << "Counting 50 to 10 (step -2):\n";
+    cout << "Counting 50 to 10 (step -2):" << endl;
     for (int i = 50; i >= 10; i -= 2) {
         cout << i << " ";
     }
-    cout << "\n\n";
+    cout << endl << endl;
 
 
-    cout << "Counting 100 to 200 (step +5):\n";
+    cout << "Counting 100 to 200 (step +5):" << endl;
     for (int i = 100; i <= 200; i += 5) {
         cout << i << " ";
     }
-    cout << "\n";
+    cout << endl;
 
     return 0;
 }
